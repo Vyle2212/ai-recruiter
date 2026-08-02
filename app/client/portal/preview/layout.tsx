bb091465cbd2ee59300f436831454812bc13f1f8
Vy <le.levy.vy@gmail.com>
2026-08-02 15:40:51 +0700

    Fix production build compatibility

--- app/client/portal/preview/layout.tsx
+++ app/client/portal/preview/layout.tsx
@@ -1,2 +1,18 @@
+import { Suspense } from "react";
 import MvpPortalLink from "./MvpPortalLink";
-export default function ClientPortalPreviewLayout({children}:{children:React.ReactNode}){return <><MvpPortalLink/>{children}</>;}
+
+export default function ClientPortalPreviewLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Suspense fallback={null}>
+        <MvpPortalLink />
+      </Suspense>
+
+      {children}
+    </>
+  );
+}
