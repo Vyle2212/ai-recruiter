d1ccb854091365737372884c7cca69c95b87548b
Vyle2212 <86584549+Vyle2212@users.noreply.github.com>
2026-09-26 04:16:05 -0700

    Package PDF standard fonts for CV extraction

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["rtf-parser-wasm"],
+  serverExternalPackages: ["rtf-parser-wasm", "pdfjs-dist"],
   outputFileTracingIncludes: {
-    "/api/**": ["./node_modules/rtf-parser-wasm/rtf_parser_bg.wasm"],
+    "/api/**": [
+      "./node_modules/rtf-parser-wasm/rtf_parser_bg.wasm",
+      "./node_modules/pdfjs-dist/package.json",
+      "./node_modules/pdfjs-dist/legacy/build/*",
+      "./node_modules/pdfjs-dist/standard_fonts/*",
+    ],
   },
 };
 
